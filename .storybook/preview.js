8c71cbb710b864fe76a87875afdd945a0df2c09b
Nicolai Garcia <nicogarcia@Nicolais-MBP.fios-router.home>
2021-01-02 20:40:02 -0500

    Successfully added Neue Haas Grotesk to storybook.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { DarkTheme } from "../styles";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={DarkTheme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
