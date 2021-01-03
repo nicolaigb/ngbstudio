9410b79a224333ac1371110f83de509787aa04e9
Nicolai Garcia <nicogarcia@Nicolais-MBP.fios-router.home>
2021-01-02 20:53:25 -0500

    Changed Project Font to Neue Haas Grotesk Text.

--- styles/DarkTheme/typography.ts
+++ styles/DarkTheme/typography.ts
@@ -1,18 +1,17 @@
 import { css } from 'styled-components';
 
-export const primaryFont = 'neue-haas-grotesk-display';
+export const primaryFont = 'neue-haas-grotesk-text';
 
 const baseTextStyle = css`
   font-family: ${primaryFont};
 `;
 
 export const Typography = {
-  displayFontFamily: 'neue-haas-grotesk-display',
-
-  textFontFamily: 'neue-haas-grotesk-text',
+  primaryFontFamily: 'neue-haas-grotesk-text',
 
   h1Style: css`
     ${baseTextStyle};
+    font-weight: bold;
     font-size: 22px;
   `,
 };
