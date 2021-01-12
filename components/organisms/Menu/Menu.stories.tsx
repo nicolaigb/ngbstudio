8a5816bd408fd9f88b9d20fc4c67da32375d01f4
Nicolai Garcia <nicogarcia@Nicolais-MBP.fios-router.home>
2021-01-12 15:27:05 -0500

    Implemented Menu Organism.

--- components/organisms/Menu/Menu.stories.tsx
+++ components/organisms/Menu/Menu.stories.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Story } from '@storybook/react';
+import { Text } from '@atoms';
 import { Menu, IMenu } from './index';
 
 export default {
   title: 'Organisms/Menu',
 };
 
-export const getMenuArgs = () => ({});
+export const getMenuArgs = () => ({
+  menuItemProps: [
+    {
+      styleType: 'regular',
+      children: <Text styleType="subheader">Home</Text>,
+    },
+    {
+      styleType: 'regular',
+      children: <Text styleType="subheader">Me</Text>,
+    },
+    {
+      styleType: 'regular',
+      children: <Text styleType="subheader">Ideas</Text>,
+    },
+    {
+      styleType: 'regular',
+      children: <Text styleType="subheader">Moooooood Board</Text>,
+    },
+  ],
+});
+
 const Template: Story<IMenu> = (args) => <Menu {...args} />;
 export const menu = Template.bind({});
 menu.args = getMenuArgs();
--- components/organisms/Menu/Menu.tsx
+++ components/organisms/Menu/Menu.tsx
@@ -1,13 +1,41 @@
-import React, { ReactElement } from 'react';
+import { Button, IButton } from '@atoms';
+import React, { ReactElement, useState } from 'react';
 import styled from 'styled-components';
 
-export interface IMenu {
+export interface IMenu extends React.HTMLAttributes<HTMLDivElement> {
+  /**
+   * List of items to be displayed in the menu
+   */
+  menuItemProps: IButton[],
 }
+
 export const Menu: React.FC<IMenu> = (
   {
+    menuItemProps,
     ...props
   },
-): ReactElement => (
-  <SMenuContainer {...props} />
-);
+): ReactElement => {
+  const [selected, setSelected] = useState<number>(0);
+
+  return (
+    <SMenuContainer {...props}>
+      {menuItemProps.map((itemProps, idx) => {
+        const onClick = () => setSelected(idx);
+        if (idx === selected) {
+          return <SSelectedButton onClick={onClick} {...itemProps} />;
+        }
+        return <SButton onClick={onClick} {...itemProps} />;
+      })}
+    </SMenuContainer>
+  );
+};
+
 const SMenuContainer = styled.div``;
+
+const SButton = styled(Button)`
+  color: black;
+`;
+
+const SSelectedButton = styled(Button)`
+  color: red;
+`;
