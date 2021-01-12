aabfff130bf6e9942f2fdef5d3e9ea92bbb15cc2
Nicolai Garcia <nicogarcia@Nicolais-MBP.fios-router.home>
2021-01-12 11:35:00 -0500

    Changed name of wavy image.

--- components/atoms/Image/Image.stories.tsx
+++ components/atoms/Image/Image.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import wavy from 'assets/cant-grow-exclusively-from-anguis-unless-we-cherish-every-moment.jpg';
+import wavy from 'assets/cant-grow-exclusively-from-anguish-unless-we-cherish-every-moment.jpg';
 import { Image, IImage } from './index';
 
 export default {
