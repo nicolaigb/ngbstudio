a2d7f15a459de57e352771b9942d710df9eb97e1
Nicolai Garcia <nicogarcia@Nicolais-MBP.fios-router.home>
2021-01-14 20:57:15 -0500

    Added default backgrounds to storybook.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,19 @@ import { ThemeProvider } from "styled-components";
 import { DarkTheme } from "../styles";
 
 export const parameters = {
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#343740'
+      },
+      {
+        name: 'light',
+        value: '#FFFFFF'
+      }
+    ]
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
