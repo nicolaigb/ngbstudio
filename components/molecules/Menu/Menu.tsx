749a12f863db4f4fcc2154e3fac3d49c22f5be3e
Nicolai Garcia <nicogarcia@Nicolais-MBP.fios-router.home>
2021-01-22 17:48:52 -0500

    Turn menu into a row on mobile.

--- components/molecules/Menu/Menu.tsx
+++ components/molecules/Menu/Menu.tsx
@@ -1,10 +1,10 @@
 import {
-  IInternalLink, InternalLink, VerticalList, IVerticalList,
+  IInternalLink, InternalLink,
 } from '@atoms';
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
 
-export interface IMenu extends IVerticalList {
+export interface IMenu extends React.HTMLAttributes<HTMLDivElement> {
   /**
    * List of items to be displayed in the menu
    */
@@ -19,11 +19,35 @@ export const Menu: React.FC<IMenu> = (
 ): ReactElement => (
   <SMenuContainer {...props}>
     {menuItemProps.map(
-      (internalLinkProps, idx) => <InternalLink key={idx} {...internalLinkProps} />,
+      (internalLinkProps, idx) => (
+        <SMenuItemContainer>
+          <InternalLink key={idx} {...internalLinkProps} />
+        </SMenuItemContainer>
+      ),
     )}
   </SMenuContainer>
 );
 
-const SMenuContainer = styled(VerticalList)`
-  justify-content: flex-start;
+const SMenuContainer = styled.div`
+  display: flex;
+  @media only screen and (max-width: ${({ theme }) => theme.Spacing.mobileMax}) {
+    flex-direction: row;
+    justify-content: flex-start;
+    overflow-x: scroll;
+  }
+
+  @media only screen and (min-width: ${({ theme }) => theme.Spacing.webMin}) {
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+`;
+
+const SMenuItemContainer = styled.div`
+  @media only screen and (max-width: ${({ theme }) => theme.Spacing.mobileMax}) {
+    margin-right: ${({ theme }) => theme.Spacing.wide};
+  }
+
+  @media only screen and (min-width: ${({ theme }) => theme.Spacing.webMin}) {
+    margin-bottom: ${({ theme }) => theme.Spacing.regular}
+  }
 `;
