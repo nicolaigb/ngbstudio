fb40c75d41f4f3c8e0e6ef1365004bf95f8d673b
Nicolai Garcia <nicogarcia@Nicolais-MBP.fios-router.home>
2021-02-05 16:26:28 -0500

    Styled modal.

--- components/atoms/Modal/Modal.stories.tsx
+++ components/atoms/Modal/Modal.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Text } from '@atoms';
 import { Story } from '@storybook/react';
 import { Modal, IModal } from './index';
 
@@ -8,6 +9,7 @@ export default {
 
 export const getModalArgs = () => ({
   isOpen: false,
+  children: <Text styleType="title">HELLO, WORLD!</Text>,
 });
 const Template: Story<IModal> = (args) => <Modal {...args} />;
 export const modal = Template.bind({});
--- components/atoms/Modal/Modal.tsx
+++ components/atoms/Modal/Modal.tsx
@@ -6,12 +6,21 @@ export interface IModal extends ReactModal.Props {}
 
 export const Modal: React.FC<IModal> = (
   {
+    children,
     ...props
   },
 ): ReactElement => (
-  <SModal {...props}>
-    HELLO
+  <SModal {...props} style={{ overlay: { backgroundColor: 'rgba(52, 55, 64, 0.85)' } }}>
+    {children}
   </SModal>
 );
 
-const SModal = styled(ReactModal)``;
+const SModal = styled(ReactModal)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow-y: hidden;
+  padding: 16px;
+  outline: none;
+`;
