de7d1c557ab9f96cc31abdd7a9fec363394a15a9
Nicolai Garcia <nicogarcia@Nicolais-MBP.fios-router.home>
2021-07-20 00:22:20 -0400

    Added redux store and provider.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import store from 'src/app/store';
+import { Provider } from 'react-redux';
 import { DarkTheme } from '../styles';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={DarkTheme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={DarkTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
