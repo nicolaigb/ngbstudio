29fb762a662d824ff1e564f5ea5c6dd1a811d4e2
ng <nicog98@gmail.com>
2022-11-23 14:02:11 -0800

    Gutted ideas mood and me pages.

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,6 +1,5 @@
 export * from './Dates';
 export * from './Header';
-export * from './InformationEntry';
 export * from './Masonry';
 export * from './ImageModal';
 export * from './TextEntry';
--- components/templates/index.ts
+++ components/templates/index.ts
@@ -1,6 +1,3 @@
-export * from './Ideas';
 export * from './Layout';
-export * from './Me';
-export * from './MoodBoard';
 export * from './WorkDetail';
 export * from './WorksGrid';
