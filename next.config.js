8d37c56e2d06cc7ec021bd82eb68feeaeecd2b3f
ng <nicog98@gmail.com>
2023-04-05 23:41:53 -0400

    Updated to latest next and react versions. Added loading blur for images.

--- next.config.js
+++ next.config.js
@@ -2,14 +2,4 @@ module.exports = {
   images: {
     domains: ['ng-web.s3.amazonaws.com'],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(png|svg|gif|jpg)$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: [{ loader: 'url-loader' }],
-    });
-    return config;
-  },
 };
