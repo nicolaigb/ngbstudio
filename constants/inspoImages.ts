66b31f2a5a1f23bf70c892b559faa0a3fe6da5ec
ng <nicog98@gmail.com>
2023-07-15 00:26:24 -0400

    Added inspo images.

--- constants/inspoImages.ts
+++ constants/inspoImages.ts
@@ -2,20 +2,34 @@ import { ContentData } from 'model';
 import { S3_BUCKET_URL } from './works';
 
 const images: ContentData[] = [
-	{
-		type: 'image',
-		src: `${S3_BUCKET_URL}/inspo/josef-albers-homage-to-the-square-arctic-bloom-1965.jpg`,
-		alt: 'Josef Albers\' Homage to the Square in a grey, light blue, and orange color scheme',
-		width: 250,
-		height: 250,
-	},
-	{
-		type: 'image',
-		src: `${S3_BUCKET_URL}/inspo/farnsworth-house.jpeg`,
-		alt: 'The Farnsworth House, designed by Ludwig Mies van der Rohe',
-		width: 500,
-		height: 281,
-	}
-]
+  {
+    type: 'image',
+    src: `${S3_BUCKET_URL}/inspo/josef-albers-homage-to-the-square-arctic-bloom-1965.jpg`,
+    alt: 'Josef Albers\' Homage to the Square in a grey, light blue, and orange color scheme',
+    width: 300,
+    height: 300,
+  },
+  {
+    type: 'image',
+    src: `${S3_BUCKET_URL}/inspo/farnsworth-house.jpeg`,
+    alt: 'The Farnsworth House, designed by Ludwig Mies van der Rohe',
+    width: 533,
+    height: 300,
+  },
+  {
+    type: 'image',
+    src: `${S3_BUCKET_URL}/inspo/kandinsky_circles.jpeg`,
+    alt: 'Artwork by Wassily Kandinsky',
+    width: 294,
+    height: 300,
+  },
+  {
+    type: 'image',
+    src: `${S3_BUCKET_URL}/inspo/jannik-sinner-gucci-wimbledon.jpeg`,
+    alt: 'Italian tennis star Jannik Sinner with a custom Gucci duffel at Wimbledon 2023',
+    width: 450,
+    height: 300,
+  },
+];
 
 export default images;
