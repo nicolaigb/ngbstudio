5c38830651c75e3abc40fc2344729a282b961db5
ng <nicog98@gmail.com>
2023-09-12 00:19:15 -0400

    Reordering quotes.

--- constants/works.ts
+++ constants/works.ts
@@ -48,7 +48,7 @@ const works: Work[] = [
     year: '2021–22',
     medium: 'Product design',
     description: '\
-      <p>This product provides accessible tools for artists to mint and distribute NFTs for free. Modeled after a &rdquo;Link in Bio&ldquo; site, a Cent Page allows creators to can style their page to their liking, and draft multi-media NFTs to promote their work or reward their audience with collectibles. Visitors to their page can then subscribe via email and collect these NFTs for free.</p>\
+      <p>This product provides accessible tools for artists to mint and distribute NFTs for free. Modeled after a &ldquo;Link in Bio&rdquo; site, a Cent Page allows creators to can style their page to their liking, and draft multi-media NFTs to promote their work or reward their audience with collectibles. Visitors to their page can then subscribe via email and collect these NFTs for free.</p>\
       ',
   },
   {
