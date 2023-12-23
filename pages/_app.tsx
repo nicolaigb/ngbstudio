a59d29cec0fa127ce43354dc0b784cdacdd6d8cc
ng <nicog98@gmail.com>
2023-12-22 17:05:42 -0800

    Added speed insights.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import store from 'src/app/store'
 import { Provider } from 'react-redux'
 import { LightTheme } from '../styles'
@@ -27,8 +28,9 @@ function MyApp({ Component, pageProps }) {
           <meta property="og:url" content="https://end-i.ng/" />
           <meta name="og:image" content="/open-graph.png" />
         </Head>
-        <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
+        <Component {...pageProps} />
       </ThemeProvider>
     </Provider>
   )
