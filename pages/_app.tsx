d892a1a13ba6714664e267ddb493a9189013d678
ng <nicog98@gmail.com>
2024-03-02 12:11:42 -0800

    Updated favicon and open graph image assets.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,18 +19,12 @@ function MyApp({ Component, pageProps }) {
           <meta property="og:title" content="NGB Studio" />
           <meta property="og:description" content="Design" />
           <meta property="og:url" content="https://ngb.studio/" />
-          <meta
-            name="og:image"
-            content="https://ng-web.s3.amazonaws.com/open-graph/ngb-studio_og.svg"
-          />
+          <meta name="og:image" content="https://ngb.studio/og.svg" />
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://ngb.studio/" />
           <meta property="twitter:title" content="NG" />
           <meta property="twitter:description" content="Design" />
-          <meta
-            property="twitter:image"
-            content="https://ng-web.s3.amazonaws.com/open-graph/ngb-studio_og.svg"
-          />
+          <meta property="twitter:image" content="https://ngb.studio/og.svg" />
         </Head>
         <Component {...pageProps} />
         <Analytics />
