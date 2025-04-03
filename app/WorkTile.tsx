7126103164322cce4b6f1d51466f25076b93af3f
ng <nicolai@ngb.studio>
2025-04-03 17:56:26 -0400

    Added title text variant.

--- app/WorkTile.tsx
+++ app/WorkTile.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { RefObject } from 'react'
 
-import { H1 } from '@/components/atoms'
+import { Title } from '@/components/atoms'
 import { getImageProps } from '@/sanity-studio/lib/image'
 import { Work } from '@/types/model'
 
@@ -25,12 +25,12 @@ export default function WorkTile({ work, ref }: WorkTileProps) {
         {imageProps && (
           <Image className="h-full w-full object-cover" {...imageProps} />
         )}
-        <H1
+        <Title
           isPlus
           className="absolute left-0 top-0 z-10 h-full w-full opacity-0 transition-all duration-200 ease-in-out group-hover:bg-white/75 group-hover:opacity-100 group-hover:backdrop-blur-sm group-hover:backdrop-contrast-150"
         >
           {title}
-        </H1>
+        </Title>
       </Link>
     </div>
   )
